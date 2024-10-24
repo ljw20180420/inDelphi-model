f4b85b828ab55ed56bfd17fde9eb977780b304c7
ljw <ljw2017@sjtu.edu.cn>
2024-10-24 18:37:10 +0800

    add precise_base precise_freq, divide freq by 100 to be ratio not percent

--- run.py
+++ run.py
@@ -2,17 +2,26 @@
 # https://github.com/maxwshen/inDelphi-model
 import sys
 import os
+import numpy as np
 
 with open(os.devnull, "w") as nu:
     # redirect build-in stdout and stderr to /dev/null
     sys.stdout = nu
     sys.stderr = nu
+    npa = np.array(["A", "C", "T", "G"])
     import inDelphi
     inDelphi.init_model(celltype = 'mESC') # Supported cell types are ['mESC', 'U2OS', 'HEK293', 'HCT116', 'K562'].
     with open(1, "w") as fd:        
-        fd.write("A\tC\tT\tG\n")
+        fd.write("A\tC\tT\tG\tprecise_base\tprecise_freq\n")
         for line in sys.stdin:
             seq, cutsite = line.strip().split()
-            pred_df, stats = inDelphi.predict(seq, int(cutsite))
-            freqs = pred_df[pred_df.Category == "ins"][['Predicted frequency']].T.values.flatten()
-            fd.write("\t".join((str(freq) for freq in freqs)) + "\n")
+            cutsite = int(cutsite)
+            pred_df, stats = inDelphi.predict(seq, cutsite)
+            freqs = pred_df[pred_df.Category == "ins"][['Predicted frequency']].T.values.flatten() / 100
+            if seq[cutsite + 4:cutsite + 6] == "GG":
+                accurate = seq[cutsite - 1]
+            elif seq[cutsite - 6:cutsite - 4] == "CC":
+                accurate = seq[cutsite]
+            else:
+                raise Exception("no pam found")
+            fd.write("\t".join((str(freq) for freq in freqs)) + f"\t{accurate}\t{freqs[npa == accurate].item()}\n")
