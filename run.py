d20a24ef80fa3ba6218c61dedc345e0975b2910a
ljw <ljw2017@sjtu.edu.cn>
2024-10-25 14:00:53 +0800

    read in from 3< write out to 4>, first argument now set celltype

--- run.py
+++ run.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 # https://github.com/maxwshen/inDelphi-model
-import sys
-import os
 import numpy as np
+import sys
+import inDelphi
 
-with open(os.devnull, "w") as nu:
-    # redirect build-in stdout and stderr to /dev/null
-    sys.stdout = nu
-    sys.stderr = nu
-    npa = np.array(["A", "C", "T", "G"])
-    import inDelphi
-    inDelphi.init_model(celltype = 'mESC') # Supported cell types are ['mESC', 'U2OS', 'HEK293', 'HCT116', 'K562'].
-    with open(1, "w") as fd:        
-        fd.write("A\tC\tT\tG\tprecise_base\tprecise_freq\n")
-        for line in sys.stdin:
-            seq, cutsite = line.strip().split()
-            cutsite = int(cutsite)
-            pred_df, stats = inDelphi.predict(seq, cutsite)
-            freqs = pred_df[pred_df.Category == "ins"][['Predicted frequency']].T.values.flatten() / 100
-            if seq[cutsite + 4:cutsite + 6] == "GG":
-                accurate = seq[cutsite - 1]
-            elif seq[cutsite - 6:cutsite - 4] == "CC":
-                accurate = seq[cutsite]
-            else:
-                raise Exception("no pam found")
-            fd.write("\t".join((str(freq) for freq in freqs)) + f"\t{accurate}\t{freqs[npa == accurate].item()}\n")
+npa = np.array(["A", "C", "T", "G"])
+
+celltype = sys.argv[1] if len(sys.argv) > 1 else 'mESC'
+inDelphi.init_model(celltype = celltype) # Supported cell types are ['mESC', 'U2OS', 'HEK293', 'HCT116', 'K562'].
+with open(3, "r") as fr, open(4, "w") as fw:        
+    fw.write("A\tC\tT\tG\tprecise_base\tprecise_freq\n")
+    for line in fr:
+        seq, cutsite = line.strip().split()
+        cutsite = int(cutsite)
+        pred_df, stats = inDelphi.predict(seq, cutsite)
+        freqs = pred_df[pred_df.Category == "ins"][['Predicted frequency']].T.values.flatten() / 100
+        if seq[cutsite + 4:cutsite + 6] == "GG":
+            accurate = seq[cutsite - 1]
+        elif seq[cutsite - 6:cutsite - 4] == "CC":
+            accurate = seq[cutsite]
+        else:
+            raise Exception("no pam found")
+        fw.write("\t".join((str(freq) for freq in freqs)) + f"\t{accurate}\t{freqs[npa == accurate].item()}\n")
